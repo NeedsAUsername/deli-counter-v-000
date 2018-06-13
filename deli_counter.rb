82883d7729b07e129088c37d95988fd2b3e28817
Kevin Chi <kevinchi118@gmail.com>
2018-06-13 15:39:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+    line = "The line is currently:"
+    if katz_deli.length == 0
+        puts "The line is currently empty"
+    else katz_deli.each_with_index do |line, index|
+        line.push(" #{index}. #{line}")
+        end
+    end
+end 
