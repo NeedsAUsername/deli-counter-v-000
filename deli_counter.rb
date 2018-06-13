0a1153c76aea987317fb1297f2ee0b346796be2d
Kevin Chi <kevinchi118@gmail.com>
2018-06-13 15:41:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     if katz_deli.length == 0
         puts "The line is currently empty."
     else katz_deli.each_with_index do |person, index|
-        line.push(" #{index}. #{person}")
+        line = line + " #{index}. #{person}"
         end
         return line
     end
